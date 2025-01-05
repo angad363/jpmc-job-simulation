5973198999aa86d27f69216307689346208bfef9
Angad Anil Gosain <angadgosain@gmail.com>
2025-01-05 11:28:19 -0800

    Completed Task 3

--- src/main/java/com/jpmc/midascore/component/TransactionListener.java
+++ src/main/java/com/jpmc/midascore/component/TransactionListener.java
@@ -1,14 +1,54 @@
 package com.jpmc.midascore.component;
 
+import com.jpmc.midascore.entity.TransactionRecord;
+import com.jpmc.midascore.entity.UserRecord;
 import com.jpmc.midascore.foundation.Transaction;
+import com.jpmc.midascore.repository.TransactionRepository;
+import com.jpmc.midascore.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TransactionListener {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TransactionRepository transactionRepository;
+
     @KafkaListener(topics = "${general.kafka-topic}", groupId = "midas-core-group")
-    public void listen(Transaction transaction){
-        System.out.println("Received Transaction:" + transaction);
+    public void handleTransaction(Transaction transaction){
+//        System.out.println("Received Transaction:" + transaction);
+        UserRecord sender = userRepository.findById(transaction.getSenderId());
+        UserRecord recipient = userRepository.findById(transaction.getRecipientId());
+
+        System.out.println("Before transaction - Sender balance: " + sender.getBalance() + ", Recipient balance: " + recipient.getBalance());
+
+        if (sender.getBalance() >= transaction.getAmount()) {
+            // Process transaction: deduct from sender and add to recipient
+            sender.setBalance(sender.getBalance() - transaction.getAmount());
+            recipient.setBalance(recipient.getBalance() + transaction.getAmount());
+
+            // Debugging point to check balances after transaction
+            System.out.println("After transaction - Sender balance: " + sender.getBalance() + ", Recipient balance: " + recipient.getBalance());
+
+            // Save the transaction record and update user balances
+            TransactionRecord record = new TransactionRecord(sender, recipient, transaction.getAmount());
+            transactionRepository.save(record);
+            userRepository.save(sender);
+            userRepository.save(recipient);
+
+            // Add a check for "waldorf" before or after each transaction
+            if (sender.getName().equals("waldorf")) {
+                System.out.println("Waldorf's balance before transaction: " + sender.getBalance());
+            }
+
+        } else {
+            // If balance is insufficient, discard transaction
+            System.out.println("Transaction failed: Insufficient balance");
+        }
+
     }
 }
